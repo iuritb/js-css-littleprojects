3b8fd2fd204aedc8f247774f491441bb9b182875
iuri.barros <iuri.t.barros@gmail.com>
2024-08-14 21:02:07 -0300

    update star rating script

--- star-rating/script.js
+++ star-rating/script.js
@@ -2,6 +2,7 @@ const stars = document.querySelectorAll(".stars i")
 
 stars.forEach((star, index1) => {
   star.addEventListener("click", () => {
+    console.log(index1 + 1)
     stars.forEach((star, index2) => {
       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active")
     })
